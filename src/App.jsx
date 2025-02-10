80b4a97920de41d27d9a0ede5ddd14b4023f8f62
ackwolver335 <abhaych335@gmail.com>
2025-02-10 22:59:14 +0530

    Router Setup Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import './App.css'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { Routes,Route } from 'react-router-dom'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
+import About from './components/About'
 
 function App() {
 
   return (
     <>
       {/* Adding a Navbar Here */}
-      <Navbar  />
-      <Home />
+      <Navbar />
+
+      {/* Setting some more Routes for different pages */}
+      <Routes>
+        <Route index element = {<Home />}/>
+        <Route path = "/about" element = {<About />} />
+      </Routes>
     </>
   )
 }
