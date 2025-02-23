c80ebe9b35498e5b279579c9baf6a63c748cb1ad
ackwolver335 <abhaych335@gmail.com>
2025-02-23 14:47:19 +0530

    HardCode Notes for Trial

--- src/context/notes/NoteState.jsx
+++ src/context/notes/NoteState.jsx
@@ -5,26 +5,33 @@ import noteContext from "./noteContext";
 // creating a method here
 const NoteState = (props) => {
 
-    const s1 = {                         // creating a state here
-        "name" : "Ack Wolver",
-        "college" : "MSI"
-    }
+    // Hardcode regarding sample notes fetching test
+    const notesInitial = [
+        {
+            "_id": "67a5bea4912a6c89cf3fa149",
+            "user": "67a4ffb4aca89731aff8ef21",
+            "title": "My Schedule for Today",
+            "description": "This is a general day description here for some of my friends.",
+            "tag": "Happy",
+            "date": "2025-02-07T08:04:52.265Z",
+            "__v": 0
+        },
+        {
+            "_id": "67bab0073f361e5d94162b1a",
+            "user": "67a4ffb4aca89731aff8ef21",
+            "title": "My Schedule for Getting",
+            "description": "This is a general day description here for some of my friends.",
+            "tag": "Happy",
+            "date": "2025-02-23T05:20:07.186Z",
+            "__v": 0
+        }
+    ]
 
-    // using the useState hook in order to setState as per our need
-    const [state,setState] = useState(s1);
+    // defining the state using useState Hook
+    const [notes, setNotes] = useState(notesInitial);
 
-    // creating a method to set the state as per our need
-    const updateState = () => {
-        setTimeout(() => {
-            setState({
-                "name" : "Abhay Chaudhary",
-                "college" : "MSIT"
-            })
-        }, 1000);
-    }
-    
     return (                                                        // boiler plate code that we always have to include here
-        <noteContext.Provider value = {{state,updateState}}>        {/* passing the state and method here regarding state change purpose */}
+        <noteContext.Provider value={{ notes, setNotes }}>
             {props.children}
         </noteContext.Provider>
     )
