066a6b26b706af28a089118425778bad698c9c4c
ackwolver335 <abhaych335@gmail.com>
2025-02-23 09:45:36 +0530

    Elements Addition regarding New Notes and Fetching Notes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import FirstComp from './FirstComp'
 import AddNote from './AddNote'
+import YourNote from './YourNote'
 
 export default function Home() {
   return (
     <div className = "xsz:pt-18 lg:pt-20 xl:pt-24 xl:pb-16">
       <FirstComp />
       <AddNote />
+      <YourNote />
     </div>
   )
 }
