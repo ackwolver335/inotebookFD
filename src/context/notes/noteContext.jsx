16b9fc6fde71a6525ee7df58d96861b05a427ba8
ackwolver335 <abhaych335@gmail.com>
2025-04-15 08:24:48 +0530

    Unusual data removed

--- src/context/notes/noteContext.jsx
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-
-// syntax for creating a particular context
-const noteContext = createContext();                        // holds the state related to notes in order access the states independent of hierarchy
-
-export default noteContext;                                 // exporting this method or context here
